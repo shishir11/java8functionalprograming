41cc8de4f795c02c2497be560a808e329f0d3dda
Shishir Sarkar <Shishir.Sarkar406@gmail.com>
2020-10-28 08:06:09 +0530

    added few example to handle boolean based map condition

--- src/com/practise/jdk8/designpattern/CommandDesignPattern.java
+++ src/com/practise/jdk8/designpattern/CommandDesignPattern.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommandDesignPattern {
-
-	public static void main(String[] args) {
-		CommandDesignPattern object = new CommandDesignPattern();
-		Map<String, Handler> commandMap = new HashMap<>();
-		commandMap.put("conditionOne", new Handler() {
+	private Map<String, Handler> commandMap = new HashMap<>();
+	private Map<Boolean, Handler> booleanCommandMap = new HashMap<>();
+	
+	public Map<String, Handler> initCommandMapHandler() {
+		commandMap.put("condition One", new Handler() {
 			@Override
 			public void handle(Object o) {
 				System.out.println("Come to the condition one...");
@@ -30,6 +30,27 @@ public class CommandDesignPattern {
 			}
 		});
 
-		commandMap.get("conditionOne").handle(object);
+		commandMap.put("condition Four", (Object o) -> System.out.println("Come to the condition two..."));
+
+		commandMap.put("condition Five", (Object o) -> {
+			String data = o.toString();
+			System.out.println("Come to the condition two..." + data);
+		});
+
+		return commandMap;
+	}
+	
+	public  Map<Boolean, Handler> initBooleanCommandMap(){
+		booleanCommandMap.put(true, (Object o) -> System.out.println("Inside true condition"));
+		booleanCommandMap.put(false, (Object o) -> System.out.println("Come to the condition false..."));
+
+		return booleanCommandMap;
+	}
+
+	public static void main(String[] args) {
+		CommandDesignPattern object = new CommandDesignPattern();
+		Map<String, Handler> initCommandMapHandler = object.initCommandMapHandler();
+
+		initCommandMapHandler.get("conditionOne").handle(object);
 	}
 }
